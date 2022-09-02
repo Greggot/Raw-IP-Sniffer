e951b401218cecab148832032a08e06fb70a46f4
Greggot <alexgerggot@gmail.com>
2022-09-02 15:17:08 +0300

    Add log to write headers to

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "main.h"
 #include "RawSocket.hpp"
+#pragma warning(disable : 4996)
 
-static inline void ReportAndExit(const char description[])
+template<class type>
+static inline void ReadHeaderFixPorts(type& header, const void* data)
 {
-    printf("%s %i\n", description, WSAGetLastError());
-    exit(1);
+    memcpy(&header, data, sizeof(header));
+    header.source = htons(header.source);
+    header.destination = htons(header.destination);
 }
 
 enum Argument
@@ -17,49 +20,50 @@ enum Argument
 
 int main(int argc, char* argv[])
 {
-    auto ip = [](const IP::Header& header) {
+    if (argc < Amount)
+    {
+        printf("Usage: RawSocket.exe <ip> <log_path>");
+        return 0;
+    }
+
+    FILE* log = fopen(argv[File], "a");
+    if (log == nullptr)
+    {
+        printf("Failed to open log...\n");
+        return 0;
+    }
+    auto ip = [&log](const IP::Header& header) {
         char from[INET_ADDRSTRLEN]{ 0 };
         char to[INET_ADDRSTRLEN]{ 0 };
 
         inet_ntop(AF_INET, &(header.source), from, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(header.destination), to, INET_ADDRSTRLEN);
 
-        printf("Version(%u) IHL(%u), DSCP(%u) ECN(%u), \n\
-            length(%u), id(%u), flags(%u), \n\
-            offset(%u), ttl(%u), protocol(%u) \n\
-            %s -> %s, raw:\n",
+        fprintf(log, "%s -> %s: ver(%u) IHL(%u), DSCP(%u) ECN(%u), length(%u), id(%u), flags(%u), offset(%u), ttl(%u), protocol(%u)\n",
+            from, to,
             header.Version, header.IHL,
             header.DSCP, header.ECN,
             header.length, header.identification,
             header.flags, header.offset,
-            header.ttl, header.protocol,
-            from, to);
+            header.ttl, header.protocol);
     };
 
-    auto udp = [](const void* data, const size_t size) {
+    auto udp = [&log](const void* data, const size_t size) {
         UDP::Header header;
-
-        memcpy(&header, data, sizeof(header));
-        header.source = htons(header.source);
-        header.destination = htons(header.destination);
+        ReadHeaderFixPorts(header, data);
         header.length = htons(header.length);
 
-        printf("UDP package: %u -> %u, length(%u), checksum(%04X)\n", header.source, header.destination, header.length, header.checksum);
+        fprintf(log, "UDP: %u -> %u, length(%u), checksum(%04X)\n\n", header.source, header.destination, header.length, header.checksum);
     };
 
-    auto tcp = [](const void* data, const size_t size) {
+    auto tcp = [&log](const void* data, const size_t size) {
         TCP::Header header;
+        ReadHeaderFixPorts(header, data);
 
-        memcpy(&header, data, sizeof(header));
-        header.source = htons(header.source);
-        header.destination = htons(header.destination);
-        printf("TCP package: %u -> %u: ", header.source, header.destination);
-
-        printf("SN(%u), ACKN(%u), offset(%u), reserved(%u)\n\
-                flags(%u %u %u %u %u %u %u %u %u), WinSize(%u),\n\
-                Checksum(%04X), URGptr(%04X) \n",
+        fprintf(log, "TCP: %u -> %u, SN(%u), ACKN(%u), offset(%u), flags(%u%u%u%u%u%u%u%u%u), WinSize(%u), Checksum(%04X), URGptr(%04X)\n\n",
+            header.source, header.destination,
             header.sequenceNumber, header.acknowledgmentNumber,
-            header.offset, header.reserved, header.NS, header.CWR, header.ECE,
+            header.offset, header.NS, header.CWR, header.ECE,
             header.URG, header.ACK, header.PSH, header.RST,
             header.SYN, header.FIN, header.windowSize,
             header.checksum, header.urgentPointer);
